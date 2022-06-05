461a33ef5cf3de073370703eecb1cdf4c7f8bec4
Brian Wangila <bwanjalah@gmail.com>
2022-06-05 23:43:28 +0300

    add call number, style and add functionality in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,20 @@
 // JS for my personal portfolio
+
+//create a pop up number display
+function openForm() {
+  document.getElementById('call').innerHTML = "0701 850 860";
+}
+
+// function closeForm() {
+//   document.getElementById("call").style.display = "none";
+// }
+
+// function openForm () {
+//   const calling = document.getElementById("call");
+//   function clickAlert() {
+//     window.alert("Call 0701850860")
+
+//   }
+//   calling.addEventListener("click", clickAlert);
+// }
+// openForm();

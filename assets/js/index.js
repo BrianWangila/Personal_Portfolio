5b88e23fc600a16fc89fd81e1054a62277df4eb1
Brian Wangila <bwanjalah@gmail.com>
2022-06-06 01:16:03 +0300

    change profile image and add functionality of contact me

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,34 @@ function openForm() {
   call.innerHTML = "0701 850 860";
 }
 
+// function sendMail() {
+//   const mail = document.getElementById("contact")
+
+// }
 
 
+// function sendEmail() {
+//   Email.send({
+//       SecureToken: "security token of your smtp",
+//       To: "ravvytek@gmail.com",
+//       From: "bwanjalah@gmail.com",
+//       Subject: "Greetings",
+//       Body: document.getElementById('text').value
+//   }).then( 
+//       message => alert("mail sent successfully")
+//   );
+// }
 
+
+function sendEmail() {
+  var link = "mailto:bwanjala@gmail.com"
+           + "?cc=ravvytek@gmail.com"
+           + "&subject=" + encodeURIComponent("Greetings")
+           + "&body=" + encodeURIComponent ("I'm interested with your portfolio") // (document.getElementById('myText').value)
+  ;
+  
+  window.location.href = link;
+}
 
 
 
